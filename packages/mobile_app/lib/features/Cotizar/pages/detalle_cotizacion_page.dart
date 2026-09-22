a81794562a44d772a5110ca739709d8cdcfc5899
Luis Aguilar Chapoñan <luisfernandoaguilarchaponan@gmail.com>
2026-09-22 16:21:45 -0500

    ci: sync candidate 73d241a92450

--- packages/mobile_app/lib/features/Cotizar/pages/detalle_cotizacion_page.dart
+++ packages/mobile_app/lib/features/Cotizar/pages/detalle_cotizacion_page.dart
@@ -66,7 +66,7 @@ class _DetalleCotizacionPageState extends ConsumerState<DetalleCotizacionPage> {
   List<SaleProcessingLine> _construirDetalles() => widget
       .carritoConPreciosFinales
       .lines
-      .map((line) => LegacySaleLineMapper.map(line, requireExactTotals: true))
+      .map((line) => SaleLinePersistenceMapper.map(line, requireExactTotals: true))
       .toList(growable: false);
 
   Future<void> _procesarCotizacion() async {
